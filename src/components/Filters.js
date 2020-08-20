948c9872554485891413dd07fc3038bc469fecd6
michaelbaud <michael.baudoin.7@gmail.com>
2020-08-21 01:19:40 +0200

    refactoring conditionnal css-class in Filters.js

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,22 +1,6 @@
-import React, { useEffect, useRef } from 'react'
+import React from 'react'
 
 const Filters = ({ activatedFilter, setActivatedFilter }) => {
-
-    const idAscButtonRef = useRef(null)
-    const idDescButtonRef = useRef(null)
-    const dateAscButtonRef = useRef(null)
-    const dateDescButtonRef = useRef(null)
-    const scoreAscButtonRef = useRef(null)
-    const scoreDescButtonRef = useRef(null)
-
-    useEffect(() => {
-        activatedFilter === 'idAsc' ? idAscButtonRef.current.classList.add("buttonFocused") : idAscButtonRef.current.classList.remove("buttonFocused")
-        activatedFilter === 'idDesc' ? idDescButtonRef.current.classList.add("buttonFocused") : idDescButtonRef.current.classList.remove("buttonFocused")
-        activatedFilter === 'dateAsc' ? dateAscButtonRef.current.classList.add("buttonFocused") : dateAscButtonRef.current.classList.remove("buttonFocused")
-        activatedFilter === 'dateDesc' ? dateDescButtonRef.current.classList.add("buttonFocused") : dateDescButtonRef.current.classList.remove("buttonFocused")
-        activatedFilter === 'scoreAsc' ? scoreAscButtonRef.current.classList.add("buttonFocused") : scoreAscButtonRef.current.classList.remove("buttonFocused")
-        activatedFilter === 'scoreDesc' ? scoreDescButtonRef.current.classList.add("buttonFocused") : scoreDescButtonRef.current.classList.remove("buttonFocused")
-    }, [activatedFilter])
 
     return (
         <div className="filtersContainer">
@@ -25,15 +9,13 @@ const Filters = ({ activatedFilter, setActivatedFilter }) => {
                 <div className="twoButtonsContainer">
                     <div>ID</div>
                     <button
-                        className="buttonLeft"
-                        ref={idAscButtonRef}
+                        className={`buttonLeft ${activatedFilter === 'idAsc' && 'buttonFocused'}`}
                         onClick={() => setActivatedFilter('idAsc')}
                     >
                         ▲
                     </button>
                     <button
-                        className="buttonRight"
-                        ref={idDescButtonRef}
+                        className={`buttonRight ${activatedFilter === 'idDesc' && 'buttonFocused'}`}
                         onClick={() => setActivatedFilter('idDesc')}
                     >
                         ▼
@@ -43,15 +25,13 @@ const Filters = ({ activatedFilter, setActivatedFilter }) => {
                 <div className="twoButtonsContainer">
                     <div>Date</div>
                     <button
-                        className="buttonLeft"
-                        ref={dateAscButtonRef}
+                        className={`buttonLeft ${activatedFilter === 'dateAsc' && 'buttonFocused'}`}
                         onClick={() => setActivatedFilter('dateAsc')}
                     >
                         ▲
                     </button>
                     <button
-                        className="buttonRight"
-                        ref={dateDescButtonRef}
+                        className={`buttonRight ${activatedFilter === 'dateDesc' && 'buttonFocused'}`}
                         onClick={() => setActivatedFilter('dateDesc')}
                     >
                         ▼
@@ -61,15 +41,13 @@ const Filters = ({ activatedFilter, setActivatedFilter }) => {
                 <div className="twoButtonsContainer">
                     <div>Score</div>
                     <button
-                        className="buttonLeft"
-                        ref={scoreAscButtonRef}
+                        className={`buttonLeft ${activatedFilter === 'scoreAsc' && 'buttonFocused'}`}
                         onClick={() => setActivatedFilter('scoreAsc')}
                     >
                         ▲
                     </button>
                     <button
-                        className="buttonRight"
-                        ref={scoreDescButtonRef}
+                        className={`buttonRight ${activatedFilter === 'scoreDesc' && 'buttonFocused'}`}
                         onClick={() => setActivatedFilter('scoreDesc')}
                     >
                         ▼
